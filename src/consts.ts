b062b94641db01565601fc057f1d7d8355b6c52e
Meredith McGee <meredith.ar.mcgee@gmail.com>
2026-02-28 19:58:14 -0500

    chore(02-01): add Substack URL constants to consts.ts
    - Added SUBSTACK_FEED_URL (meredithwritespublichealth.substack.com/feed) for build-time RSS
    - Added SUBSTACK_URL (meredithmcgee.substack.com) for user-facing links
    - RSS URL uses old publication slug which returns valid XML; profile URL is for display

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,4 @@
 export const SITE_TITLE = 'Meredith McGee, MPH';
 export const SITE_DESCRIPTION = 'Public health writer, evaluator, and researcher. Connecting community needs and data through writing.';
+export const SUBSTACK_FEED_URL = 'https://meredithwritespublichealth.substack.com/feed';
+export const SUBSTACK_URL = 'https://meredithmcgee.substack.com';
